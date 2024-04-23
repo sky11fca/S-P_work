7c366adb3e111d07c3d1cccc2e2423d19ffc74db
Bazon Bogdan <152993164+sky11fca@users.noreply.github.com>
2024-04-23 13:36:41 +0300

    Update week2_homework.R

--- week2_homework.R
+++ week2_homework.R
@@ -1,102 +1,65 @@
-#1)
+#I.3)
+
+data <- read.csv("life_expect.csv")
+
+num_bins <- 7
+
+hist(data$male, breaks=seq(min(data$male), max(data$male), length.out=num_bins+1),
+     main="Male life expectancy",
+     xlab="life expect",
+     ylab="Freq",
+     col="blue",
+     border="black")
+
+hist(data$female, breaks=seq(min(data$female), max(data$female), length.out=num_bins+1),
+     main="Female life expectancy",
+     xlab="life expect",
+     ylab="Freq",
+     col="pink",
+     border="black")
 
 
-#a)
-poison_prob <- function(lambda, k, m)
-{
-  if (k>m)
-  {
-    stop("K must be les equal to m")
-  }
+
+#III.1)
+
+outliers_mean <- function(sample) {
+  mean_val <- mean(sample)
+  std_dev <- sd(sample)
   
-  probs<-dpois(k:m, lambda)
-  return (probs)
+  outliers <- sample[abs(sample - mean_val) > 2 * std_dev]
+  
+  return(outliers)
 }
 
-geometric_prob <- function(p, k, m)
-{
-  if (k>m)
-  {
-    stop("K must be les equal to m")
-  }
-  
-  probs<-dgeom(k:m, p)
-  return (probs)
-}
+#III.2)
 
-binomial_prob <- function(n, p, k, m)
-{
-  if (k>m)
-  {
-    stop("K must be les equal to m")
-  }
-  
-  probs<-dbinom(k:m, n, p)
-  return (probs)
-}
-
-
-#b)
-
-plot_probs <- function(probs, dist_name, k, m)
-{
-  plot(k:m, probs, type="h", lwd=10)
-}
-
-#c)
-
-poisson_k0 <-function(lambda, alpha)
-{
-  k0<-qpois(1-alpha, lambda)
-  return (k0)
-}
-
-#2)
-
-
-#a)
-
-comp_freq_n_exp <- function(file_name)
-{
-  data <- read.csv("file_name")
-  
-  sample_P <- data$P
-  sample_S <- data$S
-  
-  freq_P <- as.vector(table(sample_P))/length(sample_P)
-  freq_S <- as.vector(table(sample_S))/length(sample_S)
-  
-  exp_P <- sum(as.numeric(names(freq_P))*freq_P)
-  exp_S <- sum(as.numeric(names(freq_S))*freq_S)
-  
-  return(list(freq_P = freq_P, freq_S = freq_S, exp_P = exp_P, exp_S = exp_S))
-}
-
-#b)
-
-remove_and_plot <- function(file_name, sample_name) {
-  data <- read.csv(file_name)
-  
-  sample <- data[[sample_name]]
-  
+outliers_iqr <- function(sample) {
   Q1 <- quantile(sample, 0.25)
   Q3 <- quantile(sample, 0.75)
+  
   IQR <- Q3 - Q1
   
-  low <- Q1 - 1.5 * IQR
-  up<- Q3 + 1.5 * IQR
+  outliers <- sample[sample < (Q1 - 1.5 * IQR) | sample > (Q3 + 1.5 * IQR)]
   
-  trim <- sample[sample >= low & sample <= up]
-  
-  breaks <- seq(0, 10, by = 1)
-  
-  hist(sample, breaks = breaks, main = paste("Frequency Distribution of", sample_name),
-       xlab = sample_name, ylab = "Freq", col = "lightblue", border = "black")
-  
-  hist(trimmed_sample, breaks = breaks, main = paste("Frequency Distribution of Trimmed", sample_name),
-       xlab = sample_name, ylab = "Freq", col = "lightgreen", border = "black")
-  
-  legend("topright", legend = c("Normal", "Trimmed"), fill = c("lightblue", "lightgreen"))
-  
-  return(trimmed_sample)
+  return(outliers)
 }
+
+#III.3)
+
+# Read sample data from file
+sample <- scan("sample2.txt")
+
+# Summary of the sample data
+cat("Summary of the sample data:\n")
+summary(sample)
+
+# Find outliers using mean method
+outliers_mean_result <- outliers_mean(sample)
+cat("\nOutliers using mean method:\n")
+print(outliers_mean_result)
+
+# Find outliers using 1.5 * IQR method
+outliers_iqr_result <- outliers_iqr(sample)
+cat("\nOutliers using 1.5 * IQR method:\n")
+print(outliers_iqr_result)
+
