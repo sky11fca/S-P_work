16cec2653e7ffc44cd9caf9966acfd69d79ec6e7
Bazon Bogdan <152993164+sky11fca@users.noreply.github.com>
2024-06-04 00:59:11 +0300

    Update Homework A.R

--- Homework/Homework A.R
+++ Homework/Homework A.R
@@ -1,22 +1,32 @@
 #1)
 
 #a)
-compute_poisson=function(k, m, lambda)
+Poisson=function(k, m, lambda)
 {
   prob_poisson=dpois(k:m, lambda)
   return (prob_poisson)
 }
 
-compute_geometric=function(k, m, p)
+p1=Poisson(3, 7, 0.3)
+p1
+
+Geometric=function(k, m, p)
 {
   prob_geom=dgeom(k:m, p)
   return(prob_geom)
 }
 
-compute_binomial=function(k, m, n, p)
+p2=Geometric(3, 7, 1/2)
+p2
+
+B=function(k, m, n, p)
 {
   prob_binom=dbinom(k:m, n, p)
+  return(prob_binom)
 }
+
+p3=B(3, 7, 10, 1/2)
+p3
 
 #b)
 
@@ -25,6 +35,10 @@ plot_probs=function(prob, k, m)
   x=k:m
   plot(x=x, y=prob)
 }
+
+plot_probs(p1, 3, 7)
+plot_probs(p2, 3, 7)
+plot_probs(p3, 3, 7)
 
 #c)
 
@@ -42,25 +56,9 @@ find_pos=function(lambda, threshold=1-10^6)
   return(k0-1)
 }
 
-#examples to run
-#a)
-p1=compute_poisson(3, 7, 0.3)
-p1
-
-p2=compute_geometric(3, 7, 1/2)
-p2
-
-p3=compute_binomial(3, 7, 10, 1/2)
-p3
-#B)
-plot_probs(p1, 3, 7)
-plot_probs(p2, 3, 7)
-plot_probs(p3, 3, 7)
-
-#C)
-
 k0=find_pos(3)
 k0
+
 
 
 
