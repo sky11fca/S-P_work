ac50d9147ce4c6cabbf9bbb4d092ecc86a57d311
Bazon Bogdan <152993164+sky11fca@users.noreply.github.com>
2024-06-04 08:58:54 +0300

    Update Homework D.R

--- Homework/Homework D.R
+++ Homework/Homework D.R
@@ -1,92 +1,60 @@
 #1)
 
-comp_confidence1 = function(PATH)
-{
-  data = read.csv(PATH)
-  scores = data$probabilitati
-
-  mean = mean(scores)
-  size = length(scores)
-
-  var = 92.16
-  std_dev = sqrt(var)
-
-  sem = std_dev/sqrt(size)
-
-  z_95 = qnorm(0.975)
-  z_99 = qnorm(0.995)
-
-  ci_95 = c(mean - z_95*sem, mean + z_95*sem)
-  ci_99 = c(mean - z_99*sem, mean + z_99*sem)
+zconfidence_interval = function(file_name, confidence_level) {
+  date = read.csv(file_name)
   
-  return(list(
-    mean=mean,
-    ci_95=ci_95,
-    ci_99=ci_99
-  ))
+  n = length(date$probabilitati)
+  sample_mean = mean(date$probabilitati)
+  alpha = 1 - confidence_level
+  sigma = sqrt(92.16)
+  
+  critical_z = qnorm(1 - alpha / 2, 0, 1)
+  
+  lower_bound = sample_mean - critical_z * sigma / sqrt(n)
+  upper_bound = sample_mean + critical_z * sigma / sqrt(n)
+  
+  return(c(lower_bound, upper_bound))
 }
 
-comp_confidence1('~/Documents/R/Homework/probabilitati.csv')
+cat("Interval de incredere de 95%:", zconfidence_interval("~/Documents/R/Homework/probabilitati.csv", 0.95), "\n")
+cat("Interval de incredere de 99%:", zconfidence_interval("~/Documents/R/Homework/probabilitati.csv", 0.99), "\n")
 
 #2)
 
-comp_confidence2 = function(PATH)
-
-{
-  data = read.csv(PATH)
-  scores = data$statistica
+t_conf_interval = function(file_name, confidence_level) {
+  date = read.table(file_name, header = TRUE)
   
-  mean = mean(scores)
-  size = length(scores)
+  n = length(date$statistica)
+  sample_mean = mean(date$statistica)
+  alpha = 1 - confidence_level
+  sigma = sd(date$statistica)
   
-  s=sd(scores)
-  s_mean = s/sqrt(size)
+  se = sigma / sqrt(n)
   
-  t_95=qt(0.975, df=size-1)
-  t_99=qt(0.995, df=size-1)
+  critical_t = qt(1 - alpha / 2, n - 1);
   
-  E_95=t_95*s_mean
-  E_99=t_99*s_mean
+  lower_bound = sample_mean - critical_t * se
+  upper_bound = sample_mean + critical_t * se
   
-  ci_95 = c(mean-E_95, mean+E_95)
-  ci_99 = c(mean-E_99, mean+E_99)
-
-  return(list(
-    mean=mean,
-    ci_95=ci_95,
-    ci_99=ci_99
-  ))
+  return(c(lower_bound, upper_bound))
 }
 
-comp_confidence2('~/Documents/R/Homework/statistica.csv')
-
+cat("Interval de incredere de 95%:", t_conf_interval("~/Documents/R/Homework/statistica.csv", 0.95), "\n")
+cat("Interval de incredere de 99%:", t_conf_interval("~/Documents/R/Homework/statistica.csv", 0.99), "\n")
 
 #3)
 
-nr_fail = 14
-n=100
-p_null=0.15
-
-p_hat = nr_fail/n
-z_stat = (p_hat-p_null)/sqrt(p_null*(1-p_null)/n)
-
-z_crit1 = qnorm(0.99)
-z_crit2 = qnorm(0.95)
-
-if(z_stat>z_crit1)
-{
-  cat("Yes1")
-}else
-{
-  cat("No1")
+test_proportion = function(n, alpha, succese, p0) {
+  p_prim = succese / n
+  z_score = (p_prim - p0) / sqrt(p0 * (1 - p0) / n)
+  critical_z = qnorm(1 - alpha, 0, 1)
+  
+  cat("Statistica testului", alpha * 100, "%: z_score:", z_score, "| critical_z:", critical_z)
 }
 
-cat("\n")
+n = 100
+succese = 86 
+p0 = 0.85
 
-if(z_stat>z_crit2)
-{
-  cat("Yes2")
-}else
-{
-  cat("No2")
-}
+cat(test_proportion(n, 0.01, succese, p0), "\n")
+cat(test_proportion(n, 0.05, succese, p0), "\n")
